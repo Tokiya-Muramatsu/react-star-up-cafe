13719b95d19114c163c612b66b0a47061ec37b5d
Muramatsu Tokiya <tokiya.muramatsu@g.dreamcareer.co.jp>
2024-06-05 12:55:53 +0900

    06/05 Routeを追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About/About";
+import Content from "./components/Pages/Content/Content";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/content" element={<Content />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
