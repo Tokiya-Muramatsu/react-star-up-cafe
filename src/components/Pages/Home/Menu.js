0805741e5ebef8dfba73dbd20c4cd9293bd3acf5
Muramatsu Tokiya <tokiya.muramatsu@g.dreamcareer.co.jp>
2024-06-06 14:05:18 +0900

    Menuの写真 変更

--- src/components/Pages/Home/Menu.js
+++ src/components/Pages/Home/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./menu.scss";
 
 function Menu() {
@@ -14,9 +15,9 @@ function Menu() {
         <div className="menu-wrap">
           <div className="menu">
             <div>
-              <img src="./img/slider06.jpg" alt="" />
+              <img src="./img/menu01.jpg" alt="" />
             </div>
-            <div>
+            <div className="text-box">
               <h4>コーヒー</h4>
               <p>300円</p>
               <p>
@@ -28,9 +29,23 @@ function Menu() {
 
           <div className="menu">
             <div>
-              <img src="./img/slider06.jpg" alt="" />
+              <img src="./img/menu02.jpg" alt="" />
             </div>
+            <div className="text-box">
+              <h4>アイスコーヒー</h4>
+              <p>300円</p>
+              <p>
+                ブラジル産の豆を使用した、上品な香りが特徴のコーヒーです。
+                一口で広がる豊かな風味をお楽しみください。
+              </p>
+            </div>
+          </div>
+
+          <div className="menu">
             <div>
+              <img src="./img/menu03.jpg" alt="" />
+            </div>
+            <div className="text-box">
               <h4>コーヒー</h4>
               <p>300円</p>
               <p>
@@ -42,9 +57,9 @@ function Menu() {
 
           <div className="menu">
             <div>
-              <img src="./img/slider06.jpg" alt="" />
+              <img src="./img/menu04.jpg" alt="" />
             </div>
-            <div>
+            <div className="text-box">
               <h4>コーヒー</h4>
               <p>300円</p>
               <p>
@@ -56,9 +71,9 @@ function Menu() {
 
           <div className="menu">
             <div>
-              <img src="./img/slider06.jpg" alt="" />
+              <img src="./img/menu05.jpg" alt="" />
             </div>
-            <div>
+            <div className="text-box">
               <h4>コーヒー</h4>
               <p>300円</p>
               <p>
@@ -70,9 +85,9 @@ function Menu() {
 
           <div className="menu">
             <div>
-              <img src="./img/slider06.jpg" alt="" />
+              <img src="./img/menu06.jpg" alt="" />
             </div>
-            <div>
+            <div className="text-box">
               <h4>コーヒー</h4>
               <p>300円</p>
               <p>
@@ -81,21 +96,16 @@ function Menu() {
               </p>
             </div>
           </div>
-          
-          <div className="menu">
-            <div>
-              <img src="./img/slider06.jpg" alt="" />
-            </div>
-            <div>
-              <h4>コーヒー</h4>
-              <p>300円</p>
-              <p>
-                ブラジル産の豆を使用した、上品な香りが特徴のコーヒーです。
-                一口で広がる豊かな風味をお楽しみください。
-              </p>
-            </div>
-          </div>
+        </div>
 
+        <div className="my-20">
+          <p className="m-auto text-center">
+            <Link to="/menu" className="text-center">
+              <span className="text-xl py-6 px-20 bg-lime-200 rounded-full hover:bg-lime-500">
+                VIEW MORE <i className="fa-solid fa-arrow-right"></i>
+              </span>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
